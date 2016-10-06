a0b37dbbd5c3c0fb1fda89443d0672f4f2b2492c
TSKumarage <tskumarage@gmail.com>
2016-10-06 16:59:19 +0530

    Tests on new MUSK dataset

--- h2o_autoencoder/anomaly _detection_1.py
+++ h2o_autoencoder/anomaly _detection_1.py
@@ -5,34 +5,54 @@ import h2o.model.metrics_base
 import numpy as np
 from h2o.estimators.deeplearning import H2OAutoEncoderEstimator
 
+
 def main():
     os.environ['NO_PROXY'] = 'localhost'
 
     # Start H2O on your local machine
     h2o.init()
 
-    # train_data=h2o.import_file("/home/wso2123/My Work/H2O_Anomaly/mnist/train.csv")
-    # test_data=h2o.import_file("/home/wso2123/My Work/H2O_Anomaly/mnist/test.csv")
-    data_set1="/home/wso2123/My Work/Datasets/Breast cancer wisconsin/data.csv"
+    # Dataset paths
+    mnist_data_train="/home/wso2123/My Work/H2O_Anomaly/mnist/train.csv"
+    mnist_data_test="/home/wso2123/My Work/H2O_Anomaly/mnist/test.csv"
+    cancer_data="/home/wso2123/My Work/Datasets/Breast cancer wisconsin/data.csv"
+    musk_clean1="/home/wso2123/My Work/Datasets/Musk/clean1.data"
+    musk_clean2 = "/home/wso2123/My Work/Datasets/Musk/clean2.data"
+    ionosphere_data="/home/wso2123/My Work/Datasets/Ionosphere/ionosphere.csv"
 
+    # musk_frame1=h2o.import_file(musk_clean1)
+    # musk_frame2=h2o.import_file(musk_clean2)
+    #
+    # print "hello"
+    #
+    # full_frame=musk_frame1.rbind(musk_frame2)
+    #dataset currently used for anomaly detection
+    current_datraset=cancer_data
 
-    full_frame = h2o.import_file(data_set1)
+    response_variable=0;
+    full_frame = h2o.import_file(current_datraset)
 
 
     # Split the data Frame into two random frames according to the given ratio
     (train_data, test_data) = full_frame.split_frame([0.7])
+    train_data=get_train_frame(full_frame,int(len(full_frame)*0.7))
 
     #
     # Train deep autoencoder learning model on "normal"
     # training data, y ignored
     #
+
+    # print test_data[len(full_frame.col_names)-1]
+
     anomaly_model = H2OAutoEncoderEstimator(
         activation="Tanh",
-        hidden=[25, 12, 25],
+        hidden=[25,12,25],
         sparse=True,
         l1=1e-4,
         epochs=100,
+
     )
+
     #
     #
     #
@@ -41,17 +61,23 @@ def main():
     # mat.show()
     #
     #
+    recon_error = anomaly_model.anomaly(train_data, False)
+    error_str = recon_error.get_frame_data()
+
+    err_list = map(float, error_str.split("\n")[1:-1])
+    max_err=max(err_list)
+    print "Max error in training data:",max(err_list)
     print "anomaly model train mse: ", anomaly_model.mse()
     # Compute reconstruction, error with the Anomaly
     # detection app (MSE between output and input layers)
     recon_error = anomaly_model.anomaly(test_data, False)
     error_str=recon_error.get_frame_data()
-    print error_str.split("\n")[0]
 
     err_list=map(float,error_str.split("\n")[1:-1])
     quntile=0.95
     #
-    threshold = get_percentile_threshold(quntile,err_list)
+    threshold = max_err
+        # get_percentile_threshold(quntile,err_list)
 
     print "Quntile used: ", quntile
     print "The following test points are reconstructed with an error greater than: ",threshold
@@ -61,20 +87,20 @@ def main():
     tn = 0
     fn = 0
     cnt = 0
-    str=test_data.get_frame_data()
-    list=str.split("\n")
-    print
+
+    list=test_data["diagnosis"]
+
     for i in range(len(recon_error) - 1):
         if(err_list[i]>threshold):
-            if(list[i+1].split(",")[-1]=="\"normal.\""):
+            if(list[i,0]=="b"):
                 fp=fp+1
             else:
                 tp=tp+1
         else:
-            if (list[i+1].split(",")[-1]=="\"normal.\""):
-                tn = tn + 1
-            else:
+            if (list[i,0]=="b"):
                 fn = fn + 1
+            else:
+                tn = tn + 1
 
     print "max: ", max(err_list)
     print "TP :", tp, "/n"
@@ -93,6 +119,19 @@ def get_percentile_threshold(quntile,data_frame):
     var = np.array(data_frame)  # input array
     return np.percentile(var, quntile*100)
 
+def get_train_frame(frame,size):
+    list = frame["diagnosis"]
+    new_frame=[]
+    for i in range(len(list)):
+        if(list[i,0]=="g"):
+            if len(new_frame)==0:
+                new_frame=frame[i,0:]
+            else:
+                new_frame=new_frame.rbind(frame[i,0:])
+    if len(new_frame)>size:
+        return new_frame[0:size,0:]
+    else:
+        return new_frame
 
 
 if __name__ == '__main__':
