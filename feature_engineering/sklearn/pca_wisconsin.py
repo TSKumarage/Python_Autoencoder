eb2a19ea62936d157ef2fbefba61623ab85c6bc8
TSKumarage <tskumarage@gmail.com>
2016-10-28 16:20:17 +0530

    PCA on wisconsin dataset completed

--- feature_engineering/sklearn/pca_wisconsin.py
+++ feature_engineering/sklearn/pca_wisconsin.py
@@ -1,47 +1,101 @@
 # Load the Pima Indians diabetes dataset from CSV URL
 import numpy as np
-import urllib
 import pandas as pd
-from sklearn import datasets
 from sklearn import decomposition
 from sklearn_pandas import DataFrameMapper
 from sklearn import preprocessing
-iris = datasets.load_iris()
+from sklearn import metrics
 
-complete_data = "/home/wso2123/My Work/Datasets/Breast cancer wisconsin/data.csv"
-train_data = "/home/wso2123/My Work/Datasets/Breast cancer wisconsin/uncorrected_train.csv"
-validate_data = "/home/wso2123/My Work/Datasets/Breast cancer wisconsin/validate.csv"
-test_data = "/home/wso2123/My Work/Datasets/Breast cancer wisconsin/test.csv"
-
-# load the CSV file as a numpy matrix
-complete_frame = pd.read_csv(complete_data)
-train_frame = pd.read_csv(train_data)
-validate_frame = pd.read_csv(validate_data)
-test_frame = pd.read_csv(test_data)
+global complete_frame
+global train_frame
+global validate_frame
+global test_frame
+global d
+global d1
 
 
-train_frame = pd.get_dummies(train_frame)
-feature_list = list(train_frame.columns)
-mapper = DataFrameMapper([(feature_list[-2:], preprocessing.OneHotEncoder()), (feature_list[:-2], None)])
-d = mapper.fit_transform(train_frame)
+def main():
+    global complete_frame
+    global train_frame
+    global validate_frame
+    global test_frame
+    global d
+    global d1
 
-test_frame = pd.get_dummies(test_frame)
-feature_list = list(test_frame.columns)
-mapper = DataFrameMapper([(feature_list[-2:], preprocessing.OneHotEncoder()), (feature_list[:-2], None)])
-d1 = mapper.fit_transform(test_frame)
-print d
+    complete_data = "/home/wso2123/My Work/Datasets/Breast cancer wisconsin/data.csv"
+    train_data = "/home/wso2123/My Work/Datasets/Breast cancer wisconsin/uncorrected_train.csv"
+    validate_data = "/home/wso2123/My Work/Datasets/Breast cancer wisconsin/validate.csv"
+    test_data = "/home/wso2123/My Work/Datasets/Breast cancer wisconsin/test.csv"
 
-#
-# print d
-#
-# mapper = DataFrameMapper([(list(test_frame.columns), preprocessing.OneHotEncoder())])
-# d1 = mapper.fit_transform(test_frame)
-# print d1
-sp_pcs = decomposition.SparsePCA(n_components=9)
-sp_pcs.fit(d)
-code = sp_pcs.transform(d1)
-print len(d), len(d1)
-print len(code)
-# dataset = np.loadtxt(complete_data, delimiter=",")
+    # load the CSV file as a numpy matrix
+    complete_frame = pd.read_csv(complete_data)
+    train_frame = pd.read_csv(train_data)
+    validate_frame = pd.read_csv(validate_data)
+    test_frame = pd.read_csv(test_data)
 
-# separate the data from the target attributes
+    train_frame = pd.get_dummies(train_frame)
+    feature_list = list(train_frame.columns)
+    mapper = DataFrameMapper([(feature_list[-2:], preprocessing.OneHotEncoder()), (feature_list[:-2], None)])
+    d = mapper.fit_transform(train_frame)
+
+    test_frame = pd.get_dummies(test_frame)
+    feature_list = list(test_frame.columns)
+    mapper = DataFrameMapper([(feature_list[-2:], preprocessing.OneHotEncoder()), (feature_list[:-2], None)])
+    d1 = mapper.fit_transform(test_frame)
+
+    for i in range(1):
+        print i, "---------------"
+        model_build(12)
+
+
+def model_build(i):
+
+    sp_pcs = decomposition.PCA(n_components=i)
+    sp_pcs.fit(d)
+    code = sp_pcs.transform(d1)
+    out_put = sp_pcs.inverse_transform(code)
+
+    print d1
+    print out_put
+
+    recons_err = []
+    for i in range(len(d1)):
+        recons_err.append(metrics.mean_squared_error(d1[i], out_put[i]))
+
+    tp = 0
+    fp = 0
+    tn = 0
+    fn = 0
+    lbl_list = test_frame["diagnosis_B"]
+    quntile = 0.65
+
+    threshold = get_percentile_threshold(quntile, recons_err)
+    for i in range(len(recons_err)):
+        if recons_err[i] > threshold:
+            if lbl_list[i] == 1:
+                fp += 1
+            else:
+                tp += 1
+        else:
+            if lbl_list[i] == 1:
+                tn += 1
+            else:
+                fn += 1
+
+    recall = 100 * float(tp) / (tp + fn)
+    print "Threshold :", threshold
+    print "TP :", tp, "/n"
+    print "FP :", fp, "/n"
+    print "TN :", tn, "/n"
+    print "FN :", fn, "/n"
+    print "Recall (sensitivity) true positive rate (TP / (TP + FN)) :", recall
+    print "Precision (TP / (TP + FP) :", 100 * float(tp) / (tp + fp)
+    print "F1 score (harmonic mean of precision and recall (sensitivity)) (2TP / (2TP + FP + FN)) :", 200 * float(tp) / (2 * tp + fp + fn)
+
+
+def get_percentile_threshold(quntile, data_frame):
+    var = np.array(data_frame)  # input array
+    return np.percentile(var, quntile*100)
+
+if __name__ == '__main__':
+    main()
