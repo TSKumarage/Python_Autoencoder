a861b65790f25804c2e0069368398fea66400379
TSKumarage <tskumarage@gmail.com>
2016-12-15 15:09:02 +0530

    data preprocessorr completed.

--- h2o_autoencoder/preprocessor.py
+++ h2o_autoencoder/preprocessor.py
@@ -1,121 +1,64 @@
-import h2o
-import os
-import h2o.frame
-import h2o.model.metrics_base
 import pandas as pd
 from tqdm import tqdm
 
 
 def main():
     # Dataset paths
+    full_data = "/home/wso2123/My  Work/Datasets/Creditcard/creditcard.csv"
+    dir_path = "/home/wso2123/My  Work/Datasets/Creditcard"
 
-    inputFileName = "/home/wso2123/My  Work/Datasets/Creditcard/creditcard.csv"
-    outputFileName = "/home/wso2123/My  Work/Datasets/Creditcard/creditcard_normal.csv"
+    full_frame = pd.read_csv(full_data)
 
-    # cancer_data = "/home/wso2123/My Work/Datasets/Breast cancer wisconsin/data.csv"
-    # cancer_data_normal = "/home/wso2123/My Work/Datasets/Breast cancer wisconsin/Breast cancer wisconsin_normal.csv"
-    # musk_clean1 = "/home/wso2123/My Work/Datasets/Musk/clean1.data"
-    # musk_clean2 = "/home/wso2123/My Work/Datasets/Musk/clean2.data"
-    # ionosphere_data = "/home/wso2123/My Work/Datasets/Ionosphere/ionosphere.csv"
-    # kddcup_data_set1 = "/home/wso2123/My Work/Datasets/KDD Cup/kddcup.data.corrected"
-    # kddcup_data_set1_normal = "/home/wso2123/My Work/Datasets/KDD Cup/kddcup.data.corrected_normal.csv"
-    # kddcup_data_set2 = "/home/wso2123/My Work/Datasets/KDD Cup/kddcup.data.corrected"
-    #
-    # complete_data = "/home/wso2123/My Work/Datasets/KDD Cup/train.csv"
-    # train_data = "/home/wso2123/My Work/Datasets/KDD Cup/uncorrected_train.csv"
-    # validate_data = "/home/wso2123/My Work/Datasets/KDD Cup/validate.csv"
-    # test_data = "/home/wso2123/My Work/Datasets/KDD Cup/test.csv"
+    # prepare normal data file
+    split_normal_data(full_frame, dir_path, 31, "0")
 
-    # os.environ['NO_PROXY'] = 'localhost'
-    #
-    # # Start H2O on your local machine
-    # h2o.init()
+    # open normal file
+    normal_frame = pd.read_csv(dir_path+"normal.csv")
 
-    # # load the CSV file as a numpy matrix
-    # complete_frame = h2o.import_file(complete_data)
-    # train_frame = h2o.import_file(train_data)
-    # validate_frame = h2o.import_file(validate_data)
-    # test_frame = h2o.import_file(test_data)
-    #
-    # h2o.export_file(complete_frame, complete_data, force=True)
-    # h2o.export_file(train_frame, train_data, force=True)
-    # h2o.export_file(test_frame, test_data, force=True)
-    # h2o.export_file(validate_frame, validate_data, force=True)
+    # split data into train test validate frames
+    split_datasets(full_frame, normal_frame, dir_path)
 
+# prepare test, train and validate data sets
+def split_datasets(full_frame, normal_frame, dir_path):
 
-    full_frame = pd.read_csv(inputFileName)
-    print full_frame.iloc[0:1, :]
-    print len(full_frame)
-    normal_frame = get_pandas_frame(full_frame, 0)
-    normal_frame.to_csv(outputFileName, index=False)
+    sample_ratio = 1 - (len(full_frame) * 0.7) / len(normal_frame)
 
-    # # get_normal_frame(full_frame, len(full_frame.columns)-1, )
-    # # uncorrected_train_frame.to_csv("/home/wso2123/My Work/Datasets/KDD Cup/f_uncorrected_train.csv", index=False)
-    # #full_frame.to_csv("/home/wso2123/My Work/Datasets/KDD Cup/kddcup.data_10_percent_corrected_alpha.csv", index=False)
-    # # normal_frame = get_"pandas_frame(full_frame, "normal.")
-    # # normal_frame.to_csv("/home/wso2123/My Work/Datasets/KDD Cup/kddcup.data_10_percent_corrected_normal.csv")
-    # print "Done"
-    # #print normal_frame
-    # # anomaly_frame = get_anomaly_frame(full_frame,"C35","g")
-    #
-    # #h2o.export_file(normal_frame, "/home/wso2123/My Work/Datasets/KDD Cup/kddcup.data_10_percent_corrected_normal.csv")
-    # # h2o.export_file(anomaly_frame, "/home/wso2123/My Work/Datasets/Ionosphere/Ionosphere_anomaly.csv")
-    # # test_frame = np.random.choice(full_frame., int(len(full_frame)*0.7))
-    # # print test_frame
+    if sample_ratio < 0.1:
+        validate_frame = normal_frame.sample(frac=0.1, random_state=200)
+    else:
+        validate_frame = normal_frame.sample(frac=sample_ratio, random_state=200)
+
+    train_frame = normal_frame.drop(validate_frame.index)
+
+    test_frame = get_test_frame(full_frame, 0.3)
+    uncorrected_train_frame = full_frame.drop(test_frame.index)
+    validate_frame.to_csv(dir_path + "/validate.csv", index=False)
+    train_frame.to_csv(dir_path + "/train.csv", index=False)
+    test_frame.to_csv(dir_path + "/test.csv", index=False)
+    uncorrected_train_frame.to_csv(dir_path + "/uncorrected_train.csv", index=False)
 
 
 def get_test_frame(frame, sample_ratio):
     return frame.sample(frac=sample_ratio, random_state=200)
 
 
-def get_normal_frame(frame, response_var, lbl):
+# Separate normal data from the full frame and write it to a separate file
+def split_normal_data(full_frame, dir_path, response_index, normal_lbl):
 
-    lbl_list = frame[response_var]
-    new_frame = []
-    for i in tqdm(range(len(lbl_list))):
-        if lbl_list[i, 0] == lbl:
-            if len(new_frame) == 0:
-                new_frame = frame[i, 0:]
-            else:
-                new_frame = new_frame.rbind(frame[i, 0:])
-    return new_frame
+    normal_frame = get_pandas_frame(full_frame, response_index, normal_lbl)
+    normal_frame.to_csv(dir_path + "/normal.csv", index=False)
 
 
-def get_anomaly_frame(frame, response_var, lbl):
-
-    lbl_list = frame[response_var]
-    new_frame = []
-    for i in range(len(lbl_list)):
-        if lbl_list[i, 0] != lbl:
-            if len(new_frame) == 0:
-                new_frame = frame[i, 0:]
-            else:
-                new_frame = new_frame.rbind(frame[i, 0:])
-    return new_frame
-
-
-def get_pandas_frame(frame, lbl):
-    lbl_list = frame.iloc[:, -1]
-    new_frame = frame.iloc[1016, :]
+# Return a pandas frame with only the corresponding lbl
+def get_pandas_frame(frame, res_index, lbl):
+    lbl_list = frame.iloc[:, res_index]
+    new_frame = frame.iloc[0, :]
     for i in tqdm(range(1, len(lbl_list))):
         if lbl_list.iloc[i] == lbl:
             df1 = frame.iloc[i, :]
             new_frame = new_frame.append(df1)
     return new_frame
 
-
-def get_train_frame(frame):
-    str_data = frame.get_frame_data()
-    lbl_list = str_data.split("\n")
-    # lbl_list = frame["C42"]
-    test_frame=pd.DataFrame(frame)
-    new_frame = pd.DataFrame()
-
-    for i in tqdm(range(len(lbl_list))):
-        if lbl_list[i].split(",")[-1] == "\"normal.\"":
-            df1 = test_frame.iloc[i, :]
-            new_frame = new_frame.append(df1)
-    return new_frame
 
 if __name__ == '__main__':
     main()
